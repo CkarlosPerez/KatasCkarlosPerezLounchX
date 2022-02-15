2c4ad39b6543949357cf34c48cd674cd5c9033d2
Carlos Perez Ramirez <99229278+CkarlosPerez@users.noreply.github.com>
2022-02-15 00:03:18 -0600

    Ultimo cambio para entrega

---
+++ open.py
@@ -0,0 +1,18 @@
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+
+if __name__ == '__main__':
+    main()
+
+def water_left(astronauts, water_left, days_left):
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+    
+water_left(5, 100, 2)
